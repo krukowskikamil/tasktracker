ad5e3dceb4809cf37aaab981db65675a269609b2
Kamil Krukowski <kamil.krukowski02@gmail.com>
2021-07-17 18:41:42 +0200

    initializing repo plus commit code

---
+++ README.md
@@ -0,0 +1,12 @@
+Simple Time Tracking API
+
+This is REST API with postgreSQL database that allows you to track one task.
+
+To install API you need to enter the following commands: 
+
+```bash
+git clone 
+cd tasktracker
+npm install
+npm start
+```
---
+++ api.js
@@ -0,0 +1,79 @@
+const client = require("./cilent.js")
+const express = require("express")
+const bodyParser = require("body-parser")
+const { customAlphabet } = require("nanoid")
+
+const app = express()
+const nanoid = customAlphabet('1234567890', 9)
+
+app.use(bodyParser.json())
+app.listen(3333, () => {
+    console.log("Server listening at port 3333")
+})
+
+client.connect()
+
+app.get("/task", (req, res) => {
+    client.query("select * from tasks where task_status= 'ACTIVE'", (err, result) => {
+        if (!err) {
+            if(result.rowCount>0){
+                res.send(result.rows)
+            }else{
+                res.send("Currently no task is tracked")
+            }        
+        }else{
+            console.log(`Error while getting currently tracked task: ${err}`)
+        }
+    })
+    
+})
+
+app.post("/task", (req, res) => {
+    checkActiveTask().then(() => {
+        const task = req.body;
+        let insertQuery = `insert into tasks (task_id, task_name, task_creation_time, task_status) values (${nanoid()}, '${task.name}', CURRENT_TIMESTAMP, 'ACTIVE')`   
+    
+        client.query(insertQuery, (err,result) => {
+            if(!err){
+                res.send("Task added succesfully").status(201)
+                
+            }else{
+                console.log(`Error while adding new task: ${err}`)
+                res.status(500).send(`Error while adding new task: ${err}`)
+            }
+        })
+    })
+})
+
+function checkActiveTask(){
+    return new Promise((resolve, reject) => {
+        client.query("select * from tasks where task_status = 'ACTIVE'", (err, result) => {
+            if (!err) {
+                if(result.rowCount > 0){
+                    client.query(`update tasks set task_status = 'FINISHED', task_finished_time = CURRENT_TIMESTAMP where task_status = 'ACTIVE'`)
+                    resolve();
+                }else{
+                    resolve();
+                }
+            }else {
+                console.log(`Error while checking currently tracked task: ${err}`)
+                reject();
+            }
+         })
+    })
+}
+
+app.patch("/stop", (req, res) => {
+    client.query("select * from tasks where task_status = 'ACTIVE'", (err, result) => {
+        if (!err) {
+            if(result.rowCount > 0){
+                client.query(`update tasks set task_status = 'FINISHED', task_finished_time = CURRENT_TIMESTAMP where task_status = 'ACTIVE'`)
+                res.send("task stopped")
+            }else{
+                res.send("no task was tracked")
+            }
+        }else {
+            console.log(`Error while checking currently tracked task: ${err}`)
+        }
+     })
+})
